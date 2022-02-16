ec1556d3c19524607428db2e7cdd757adaa1d8b8
Ronan653 <Donnelly-r36@Ulster.ac.uk>
2022-02-16 18:02:19 +0000

    VSCode Version 3

--- SMS.web/Controllers/HomeController.cs
+++ SMS.web/Controllers/HomeController.cs
@@ -2,37 +2,41 @@
 using Microsoft.AspNetCore.Mvc;
 using SMS.web.Models;
 
-namespace SMS.web.Controllers;
-
-public class HomeController : Controller
+namespace SMS.web.Controllers
 {
-   public IActionResult Index()
-   {
-       return View();
 
-   }
+    public class HomeController : Controller
+    {
+    public IActionResult Index()
+    {
+        return View();
 
-    public IActionResult About()
-   {
+    }
 
-       //Construct viewmodel
-       var about = new AboutViewModel
-       {
-           Formed = new DateTime(2020,01,01),
+        public IActionResult About()
+    {
 
-           Title = new String("Web Based Application Development COM741"),
+        //Construct viewmodel
+        var about = new AboutViewModel
+        {
+            Formed = new DateTime(2020,01,01),
 
-           Message = new String("Teaching Staff Aiden McCaughey, Senior Lecturer School of Computing Engineering & Intelligent Systems")
+            Title = new String("Web Based Application Development COM741"),
 
-       };
+            Message = new String("Teaching Staff Aiden McCaughey, Senior Lecturer School of Computing Engineering & Intelligent Systems")
 
-       return View(about);
+        };
 
-   }
+        return View(about);
 
-   public IActionResult Privacy()
-   {
-       return View();
+    }
 
-   }
+    public IActionResult Privacy()
+    {
+        return View();
+
+    }
+    
+    }
+
 }
